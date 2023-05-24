ddbf3dc94dec8075be175240e96efbff3a212014
Nicholas Wiebe <73451654+nicholasrwx@users.noreply.github.com>
2023-05-23 22:30:11 -0600

    Update ConsumerHealthBuilder.cs

--- Builders/Udemy/FluentBuilderInheritance/ConsumerHealthBuilder.cs
+++ Builders/Udemy/FluentBuilderInheritance/ConsumerHealthBuilder.cs
@@ -1,4 +1,7 @@
-//Object that inherits from fluent builder
+//SELF is part of the Type declaration that needs to be passed in, this is why the "Builder" class isn't in it's place here
+//SELF is constrained to a type that inherits from ConsumerHealthBuilder<SELF>
+
+// Object that inherits from fluent builder
 public class ConsumerHealthBuilder<SELF> : ConsumerInfoBuilder<ConsumerHealthBuilder<SELF>> where SELF : ConsumerHealthBuilder<SELF>
 {
 
