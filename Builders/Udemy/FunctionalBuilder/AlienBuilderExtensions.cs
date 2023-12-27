47fc8e2d35c8db108521a6c61f3ecf282260b336
Nicholas Wiebe <digitalexistence18@gmail.com>
2023-12-27 15:17:54 -0700

    Backup unsaved changes

--- Builders/Udemy/FunctionalBuilder/AlienBuilderExtensions.cs
+++ Builders/Udemy/FunctionalBuilder/AlienBuilderExtensions.cs
@@ -1,4 +1,4 @@
 public static class AlienBuilderExtensions
 {
-  public static AlienBuilder WorksAs(this AlienBuilder builder, string position) => builder.Do(position => p.Position = position);
+  public static AlienBuilder WorksAs(this AlienBuilder builder, string position) => builder.Do(p => p.Position = position);
 }
