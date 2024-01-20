1178088c863c30497908821f069127e3b6fa7457
Nicholas Wiebe <73451654+nicholasrwx@users.noreply.github.com>
2024-01-20 15:44:12 -0700

    Rename AlienBuilder to GenericFluentBuilder
    Update Type Alien to Generic Type TSubject

--- Builders/Udemy/FunctionalBuilder/FluentAlienBuilder.cs
+++ Builders/Udemy/FunctionalBuilder/FluentAlienBuilder.cs
@@ -1,17 +1,17 @@
-public abstract class AlienBuilder<TSubject, TSelf> where TSelf : AlienBuilder<TSubject, TSelf> where TSubject: new()
+public abstract class GenericFluentBuilder<TSubject, TSelf> where TSelf : GenericFluentBuilder<TSubject, TSelf> where TSubject: new()
 {
     // Preserve a list of mutating functions
-    private readonly List<Func<Alien, Alien>> actions = new List<Func<Alien, Alien>>();
-    public TSelf Do(Action<Alien> action) => AddAction(action);
-    public Alien Build() => actions.Aggregate(new Alien(), (p, f) => f(p));
-    private TSelf AddAction(Action<Alien> action)
+    private readonly List<Func<TSubject, TSubject>> actions = new List<Func<TSubject, TSubject>>();
+    public TSelf Do(Action<TSubject> action) => AddAction(action);
+    public TSubject Build() => actions.Aggregate(new TSubject(), (p, f) => f(p));
+    private TSelf AddAction(Action<TSubject> action)
     {
         actions.Add(p => { action(p); return p; });
         return (TSelf) this;
     }
 }
 
-public sealed class FluentAlienBuilder : AlienBuilder<Alien, FluentAlienBuilder>
+public sealed class FluentAlienBuilder : GenericFluentBuilder<Alien, FluentAlienBuilder>
 {
     public FluentAlienBuilder Called(string name) => Do(p => p.Name = name);
 }
