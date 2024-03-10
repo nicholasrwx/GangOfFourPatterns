164c39e11b13b67b30a255d39fc89f95ca35884b
Nicholas Wiebe <73451654+nicholasrwx@users.noreply.github.com>
2024-03-10 15:59:41 -0600

    Setup ICloneable Project
    Create Person Class
    Override Person ToString Method
    Create Address Class
    Override Address ToString Method
    Create Global Using File
    Disable Nullable Property in csproj
    Implement Classes in Program

---
+++ Prototype/ICloneable/Person.cs
@@ -0,0 +1,16 @@
+public class Person
+{
+    public string[] Names;
+    public Address Address;
+
+    public Person(string[] names, Address address)
+    {
+        Names = names ?? throw new ArgumentNullException(paramName: nameof(names));
+        Address = address ?? throw new ArgumentNullException(paramName: nameof(address));
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
+    }
+}
---
+++ Prototype/ICloneable/Program.cs
@@ -0,0 +1,4 @@
+﻿var John = new Person(new[] { "John", "Doe" }, new Address("London Road", 453));
+
+Console.WriteLine(John.ToString());
+Console.WriteLine(John.Address.ToString());
