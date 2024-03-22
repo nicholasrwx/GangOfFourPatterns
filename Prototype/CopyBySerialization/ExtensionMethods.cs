883ad4af613d28a21435c99019cc6cbcf77437f6
Nicholas Wiebe <73451654+nicholasrwx@users.noreply.github.com>
2024-03-21 20:15:26 -0600

    Replace BinaryFormatter with BinaryWriter and JsonConvert
    Add GlobalUsings
    Add Newtonsoft.Json package to project
    Use JsonConvert to serialize and deserialize Objects to JSON Strings
    Use BinaryWriter to serialize and deserialize Strings to Binary
    Update Program Implementation

--- Prototype/CopyBySerialization/ExtensionMethods.cs
+++ Prototype/CopyBySerialization/ExtensionMethods.cs
@@ -1,19 +1,45 @@
+using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
 public static class ExtensionMethods
 {
     public static T DeepCopy<T>(this T self)
     {
         var stream = new MemoryStream();
-        var formatter = new BinaryFormatter();
-        formatter.Serialize(stream, self);
-        stream.Seek(0, SeekOrigin.Begin);
-        object copy = formatter.Deserialize(stream);
-        stream.Close();
-        return (T) copy;
+
+        // Boolean Specifies wether or not to leave stream open
+        // when writer is closed.
+        var writer = new BinaryWriter(stream, Encoding.UTF8, true);
+        
+        // Serialize an Object to a JSON String
+        var serializedObject = JsonConvert.SerializeObject(self);
+
+        // Convert to Binary and Write to Memory Stream
+        writer.Write(serializedObject);
+
+        // Reset the position in the current stream
+        writer.Seek(0, SeekOrigin.Begin);
+        
+        // Close the writer ( and stream if leaveOpen flag = false )
+        writer.Close();
+
+        // Read From Stream
+        var reader = new BinaryReader(stream, Encoding.UTF8, false);
+
+        // Convert Binary back to String representation
+        var result = reader.ReadString();
+
+        // Convert JSON String back to an Object        
+        return JsonConvert.DeserializeObject<T>(result);
     }
 
+    /*
     public static T DeepCopyXml<T>(this T self)
     {
         return (T) copy;
     }
+    */
 
 }
--- Prototype/CopyBySerialization/Program.cs
+++ Prototype/CopyBySerialization/Program.cs
@@ -1,24 +1,16 @@
 ﻿using System;
-using System.Net.Sockets;
 using static System.Console;
 
-// This is a way to perform partial deep copies through inheritance
-// and casting to parent types
-
-
-var john = new Employee();
+var john = new Person();
 john.Names = new string[] { "Jason", "Corey" };
 john.Address = new Address { HouseNumber = 435, StreetName = "Gordon Rd. SE" };
 
-var copy = john.DeepCopy();
+var tony = john.DeepCopy();
 
-copy.Names[1] = "Smith";
-copy.Address.HouseNumber++;
-copy.Salary = 7875550;
+tony.Names[0] = "Tony";
+tony.Names[1] = "Smith";
+tony.Address.HouseNumber++;
 
-// these may have been removed
-Employee e = john.DeepCopy<Employee>();
-Person p = john.DeepCopy<Person>();
 
 Console.WriteLine(john);
-Console.WriteLine(copy);
+Console.WriteLine(tony);
