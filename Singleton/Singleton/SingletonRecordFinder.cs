238c3b3a001ba057ee14ebf909acddb9578d5a72
Nicholas Wiebe <73451654+nicholasrwx@users.noreply.github.com>
2024-03-29 15:51:49 -0600

    Create a SingletonRecordFinder
    Test to ensure only a single instance is created
    Test to ensure GetTotalPopulation method works

---
+++ Singleton/Singleton/SingletonRecordFinder.cs
@@ -0,0 +1,14 @@
+﻿namespace Singleton
+{
+    internal class SingletonRecordFinder
+    {
+        // Sums up the total population for a list of cities that exist in the database
+        public int GetTotalPopulation(IEnumerable<string> names)
+        {
+            int result = 0;
+            foreach (var name in names)
+                result += SingletonDatabase.Instance.GetPopulation(name);
+            return result;
+        }
+    }
+}
