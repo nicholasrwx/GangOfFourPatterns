59b389a6f31c1ddd709cf4f2a3c2bef6da71a9fa
Nicholas Wiebe <73451654+nicholasrwx@users.noreply.github.com>
2024-03-30 13:15:29 -0600

    Demonstrate the Monostate Pattern

---
+++ Singleton/Monostate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+var ceo = new Ceo();
+ceo.Name = "Court Jennings";
+ceo.Age = 55;
+
+var ceo2 = new Ceo();
+
+WriteLine(ceo2);
+
+public class Ceo
+{
+    // Mono-state utilizes static fields and properties to have a single point of storage for state
+    private static string name;
+    private static int age;
+
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+
+    public int Age
+    {
+        get => age;
+        set => age = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+    }
+}
