18a66ab1d3b4002cad80a8919dc5102c79287f15
Nicholas Wiebe <73451654+nicholasrwx@users.noreply.github.com>
2024-04-03 02:32:57 -0600

    Create a VectorObject that extends Collection<T>
    Create a Rectangle that extends VectorObject
    Create a Point Object
    Create a Line Object
    Create a LineToPoint Adapter which generates lines out of points

---
+++ Adapter/VectorRaster/Line.cs
@@ -0,0 +1,9 @@
+﻿namespace VectorRaster
+{
+    public class Line(Point start, Point end)
+    {
+        public Point 
+            Start = start ?? throw new ArgumentNullException(nameof(start)), 
+            End = end ?? throw new ArgumentException(nameof(end));
+    }
+}
---
+++ Adapter/VectorRaster/VectorObject.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.ObjectModel;
+
+namespace VectorRaster
+{
+    public class VectorObject : Collection<Line>
+    {
+    }
+}
