52993ba28dd8c2fd0e2e927bb9d153ece0eab51e
Nicholas Wiebe <73451654+nicholasrwx@users.noreply.github.com>
2024-04-04 20:29:31 -0600

    Setup Adapter Caching Example

---
+++ Adapter/AdapterCaching/Point.cs
@@ -0,0 +1,7 @@
+﻿namespace VectorRaster
+{
+    public class Point(int x, int y)
+    {
+        public int X = x, Y = y;
+    }
+}
---
+++ Adapter/AdapterCaching/Program.cs
@@ -0,0 +1,31 @@
+﻿using VectorRaster;
+using MoreLinq;
+using static System.Console;
+
+List<VectorObject> vectorObjects
+        =
+        [
+            new VectorRectangle(1, 1, 10, 10),
+            new VectorRectangle(3, 3, 6, 6)
+        ];
+
+void DrawPoint(Point p)
+{ 
+    Write(".");
+}
+
+void Draw()
+{
+    foreach (var vo in vectorObjects)
+    {
+        foreach (var line in vo)
+        {
+            var adapter = new LineToPointAdapter(line);
+            adapter.ForEach(DrawPoint);
+            WriteLine("");
+        }
+    }
+}
+
+Draw();
+Draw();
