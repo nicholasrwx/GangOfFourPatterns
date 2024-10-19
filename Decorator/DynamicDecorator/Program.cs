3f1abd1033c579893eaee7f21b4e86a66d85863d
nicholasrwx <73451654+nicholasrwx@users.noreply.github.com>
2024-10-19 11:54:00 -0600

    Create ColoredShape and TransparentShape Types
    Implement IShape Interface on ColoredShape and TransparentShape
    Allow Both Types to Accept IShape as a Parameter
    Demonstrate Usage of Both Types in Program

--- Decorator/DynamicDecorator/Program.cs
+++ Decorator/DynamicDecorator/Program.cs
@@ -1,4 +1,13 @@
 ﻿using DynamicDecorator;
 
+// We are applying decorators upon decorators
+// Additionally, you can apply the same decorator twice
+
 var square = new Square(1.23f);
 WriteLine(square.AsString());
+
+var redSquare = new ColoredShape(square, "red");
+WriteLine(redSquare.AsString());
+
+var redHalfTransparentSquare = new TransparentShape(redSquare, 0.5f);
+WriteLine(redHalfTransparentSquare.AsString());
---
+++ Decorator/DynamicDecorator/TransparentShape.cs
@@ -0,0 +1,9 @@
+namespace DynamicDecorator;
+
+public class TransparentShape(IShape shape, float transparency) : IShape
+{
+    private IShape _shape = shape ?? throw new ArgumentNullException(paramName: nameof(shape));
+    private float _transparency = transparency;
+
+    public string AsString() => $"{shape.AsString()} has {transparency * 100.0}% transparency";
+}
