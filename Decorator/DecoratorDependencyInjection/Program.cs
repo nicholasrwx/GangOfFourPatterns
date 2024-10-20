891f581f09cbb80e23944e59444f41c0a585ebbd
nicholasrwx <73451654+nicholasrwx@users.noreply.github.com>
2024-10-20 10:13:47 -0600

    Register Container Type and Decorator
    Update Usages in Program

--- Decorator/DecoratorDependencyInjection/Program.cs
+++ Decorator/DecoratorDependencyInjection/Program.cs
@@ -1,1 +1,7 @@
 ﻿var container = new ContainerBuilder();
+container.RegisterType<ReportingService>().Named<IReportingService>("reporting");
+container.RegisterDecorator<IReportingService>((context, service) => new ReportingServiceWithLogging(service), "reporting");
+
+using var builtContainer = container.Build();
+var result = builtContainer.Resolve<IReportingService>();
+result.Report();
